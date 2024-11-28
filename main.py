384a23c84f155a9caf9e2d3f08a75cb8c379c024
VitorOF <fonseca.o.vitor@gmail.com>
2024-11-27 22:01:37 -0300

    segundo commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+print("Setup Start")
+window = pygame.display.set_mode((800, 600))
+print("Setup End")
+
+print("Loop Start")
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() #Close Window
+            quit() # end pygame
